236076e5340ea4b127fcc1452a880df2baa096fa
juno3550 <78023734+juno3550@users.noreply.github.com>
2021-04-16 22:28:08 +0800

    delete

--- run_test.py
+++
@@ -1,7 +0,0 @@
-from test_script import batch_login_script
-from test_script import batch_login_and_add_contact_script
-from conf.global_var import *
-
-
-# batch_login_script.batch_login(TEST_DATA_FILE_PATH, "chrome", "126账号")
-batch_login_and_add_contact_script.batch_login_and_add_contact(TEST_DATA_FILE_PATH, "chrome", "126账号")
